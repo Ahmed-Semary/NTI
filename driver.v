ef7352290885d55c676e0803e337ff17e6b74fdf
Ahmed-Semary <ahmedsemry5@gmail.com>
2025-08-08 00:25:13 +0300

    Add files via upload

---
+++ driver.v
@@ -0,0 +1,9 @@
+module driver #(
+    parameter WIDTH=8
+) (
+    input             data_en,
+    input  [WIDTH-1:0]data_in,
+    output [WIDTH-1:0]data_out
+);
+    assign data_out=data_en?data_in:'bz;
+endmodule
